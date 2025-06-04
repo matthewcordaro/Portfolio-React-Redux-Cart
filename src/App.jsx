db605c802e2cae7e429a019ab2dfb275bda774bb
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-06-03 23:36:08 -0400

    feat: implement async cart item fetching and loading state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { calculateTotals } from "./features/cart/cartSlice"
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice"
 import CartContainer from "./components/CartContainer"
 import Navbar from "./components/Navbar"
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
 
   const dispatch = useDispatch()
+  // Load Data
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  // Update Totals
   useEffect(() => {
     dispatch(calculateTotals())
   }, [cartItems])
 
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
     <main>
       <Navbar />
